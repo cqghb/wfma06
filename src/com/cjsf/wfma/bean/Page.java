23f2065294e765279a3f97db951d108ae935e743
王胜 <1689841661@qq.com>
2017-02-27 15:42:28 +0800

    给分页实体类的getter和setter方法添加了注释

--- src/com/cjsf/wfma/bean/Page.java
+++ src/com/cjsf/wfma/bean/Page.java
@@ -19,43 +19,74 @@ public class Page {
 	}
     
 	public Page(){}
-
+	/**
+	 * @category 获取当前页
+	 * @return 返回一个整型值(当前页)
+	 */
 	public Integer getPageIndex() {
 		return pageIndex;
 	}
-
+	/**
+	 * @category 设置当前页
+	 * @param pageIndex一个整型值(当前页)
+	 */
 	public void setPageIndex(Integer pageIndex) {
 		this.pageIndex = pageIndex;
 	}
-
+	/**
+	 * @category 获取每页显示的数据条数
+	 * @return 返回一个整型值(每页显示的数据条数)
+	 */
 	public Integer getPageSize() {
 		return pageSize;
 	}
-
+	
+	/**
+	 * @category 设置每页显示数据的条数
+	 * @param pageSize一个整型值(每页显示数据条数)
+	 */
 	public void setPageSize(Integer pageSize) {
 		this.pageSize = pageSize;
 	}
-
+	/**
+	 * @category 获取总行数
+	 * @return 返回一个整型值 (总行数)
+	 */
 	public Integer getRowTotal() {
 		return rowTotal;
 	}
-
+	/**
+	 * @category 设置总行数
+	 * @param rowTotal一个整型数值(总行数)
+	 */
 	public void setRowTotal(Integer rowTotal) {
 		this.rowTotal = rowTotal;
 	}
-
+	/**
+	 * @category 获取总页数
+	 * @return 返回一个整型值(总页数)
+	 */
 	public Integer getPageTotal() {
 		return pageTotal;
 	}
-
+	/**
+	 * @category 设置总页数
+	 * @param pageTotal 一个整型值(总行数)
+	 */
 	public void setPageTotal(Integer pageTotal) {
 		this.pageTotal = pageTotal;
 	}
-
+	/**
+	 * @category 获取起始行
+	 * @return 返回一个整型值(开始行)
+	 */
 	public Integer getStartRow() {
 		return startRow;
 	}
-
+	/**
+	 * @category 设置起始行
+	 * @param startRow 一个整型值(起始行)
+	 */
 	public void setStartRow(Integer startRow) {
 		this.startRow = startRow;
 	}
